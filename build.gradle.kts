7d7924b7b300221a9403d04c56f35d918a2d410d
Fernando Cejas <android10@fernandocejas.com>
2022-05-27 16:29:45 +0200

    feat: console app up and running.

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "com.fernandocejas.kotlin.playground"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    application
+}
+
+application {
+    mainClass.set("$group.MainKt")
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks.test { useJUnitPlatform() }
+tasks.withType<KotlinCompile>() { kotlinOptions.jvmTarget = "17" }
+
---
+++ src/main/kotlin/com/fernandocejas/kotlin/playground/Main.kt
@@ -0,0 +1,5 @@
+package com.fernandocejas.kotlin.playground
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
